f736740d45c642c4c6b9ea8fb557cd493479ef2f
Sweta Gautam <sg060479@gmail.com>
2025-11-07 03:24:22 -0500

    Time: 59 ms (97.16%), Space: 146.6 MB (77.29%) - LeetHub

--- 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.cpp
+++ 2528-maximize-the-minimum-powered-city/2528-maximize-the-minimum-powered-city.cpp
@@ -1,46 +1,32 @@
-typedef long long int ll;
-
 class Solution {
-    
-    bool IsPossible (ll min_power, const vector<ll>& default_powers, int extra_stations, int r, int num_stations) {
-        vector<ll> extra_power (num_stations+1);
-        
-        for (int j = 0; j < num_stations; j ++) {
-            extra_power[j] += (j > 0? extra_power[j-1] : 0);
-            
-            ll cur_power = default_powers[j] + extra_power[j];
-            ll required = max (0LL, min_power - cur_power);
-            if (required == 0) continue;
-            
-            if (required > extra_stations) return false;
-            extra_stations -= required;
-            
-            extra_power[j] += required;
-            extra_power[min(num_stations, j+2*r+1)] -= required;
+public:
+    bool check(vector<int> stations, long long mid, int k, int r){
+        int n = stations.size();
+        long long sum = 0;
+        for(int i = 0; i < r; i++) sum += stations[i];
+        for(int i = 0; i < n; i++){
+            if(i+r < n) sum += stations[i+r];
+            if(i-r-1 >= 0) sum -= stations[i-r-1];
+            if(sum < mid){
+                if(mid-sum > k)return false;
+                if(i+r < n)stations[i+r] += mid-sum;
+                k -= mid-sum;
+                sum = mid;
+            }
         }
-        
         return true;
     }
-    
-public:
-    long long maxPower(vector<int>& stations, int range, int k) {
-        int n = stations.size();
-     
-        vector<ll> station_powers(n+1);
-        for (int j = 0; j < n; j ++) {
-            station_powers[max(0, j-range)] += stations[j];
-            station_powers[min(n, j+range+1)] -= stations[j];
+    long long maxPower(vector<int>& stations, int r, int k) {
+        long long lo = *min_element(begin(stations),end(stations));
+        long long hi = accumulate(begin(stations),end(stations),0LL)+k;
+        long long ans = 0;
+        while(lo <= hi){
+            long long mid = lo + (hi-lo)/2;
+            if(check(stations,mid,k,r)){
+                ans = mid;
+                lo = mid+1;
+            }else hi = mid-1;
         }
-        for (int j = 1; j <= n; j ++) station_powers[j] += station_powers[j-1];
-        
-        ll l = 0, r = 1e11;
-        while (l < r) {
-            ll m = (l+r) >> 1;
-            
-            if (IsPossible (m+1, station_powers, k, range, n)) l = m+1;
-            else r = m;
-        }
-        
-        return l;
+        return ans;
     }
 };
