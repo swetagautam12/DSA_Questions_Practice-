f0205bd29e6e48dc1c5590ecd5e20536621ddf25
Sweta Gautam <sg060479@gmail.com>
2025-11-21 06:34:36 +0530

    Time: 142 ms (78.88%), Space: 16.1 MB (33.71%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int countPalindromicSubsequence(string inputString) {
-    
-    int result = 0;
-    int firstIndex[26] = {[0 ... 25] = INT_MAX};
-    int lastIndex[26] = {};
-
-    for (int i = 0; i < inputString.size(); ++i) {
-        firstIndex[inputString[i] - 'a'] = min(firstIndex[inputString[i] - 'a'], i);
-        lastIndex[inputString[i] - 'a'] = i;
+    int countPalindromicSubsequence(string s) {
+        int n = s.size();
+        vector<vector<int>> firstLastIdxes(26, vector<int>(2, -1));
+        
+        for(int i=0; i<n; i++){
+            char c = s[i];
+            if(firstLastIdxes[c-'a'][0] == -1){
+                firstLastIdxes[c-'a'][0] = i;
+            }
+            firstLastIdxes[c-'a'][1] = i;
+        }
+        int count = 0;
+        for(int i=0; i<26; i++){
+            int left = firstLastIdxes[i][0];
+            int right = firstLastIdxes[i][1];
+            unordered_set<char> charsInBetween;
+            for(int j=left+1; j<right; j++){
+                charsInBetween.insert(s[j]);
+            }
+            count += charsInBetween.size();
+        }
+        return count;
     }
-    for (int i = 0; i < 26; ++i)
-        if (firstIndex[i] < lastIndex[i])
-            result += unordered_set<char>(begin(inputString) + firstIndex[i] + 1, begin(inputString) + lastIndex[i]).size();
-    return result;
-}
 };
