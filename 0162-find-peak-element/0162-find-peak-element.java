284f54e0fef4b852d32dd6e3598c9439bf002e7b
Sweta Gautam <sg060479@gmail.com>
2026-01-23 10:57:49 +0530

    Time: 0 ms (100%), Space: 44.4 MB (10.74%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,23 +1,16 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        return peakIndexInMountainArray(nums);
 
+        int st=0,en= nums.length-1,mid=0;
+        while(st<en){
+            mid =(st+en)/2;
+            if(nums[mid]<nums[mid+1]){
+                st=mid+1;
+
+            }else{
+                en =mid;
+            }
+        }return st;
         
-    }
-    public int peakIndexInMountainArray(int[] arr){
-        int s=0;
-        int e =arr.length-1;
-        while(s<e){
-            int mid = s+ (e-s)/2;
-            if(arr[mid] >arr[mid+1]){
-                e= mid;
-
-            }
-            else{
-                s = mid+1;
-
-            }
-        }
-        return s ; 
     }
 }
