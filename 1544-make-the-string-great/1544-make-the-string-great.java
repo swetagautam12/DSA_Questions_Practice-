99a2b98f0cbc9c37e658cc05cc40c16f6921242a
Sweta Gautam <sg060479@gmail.com>
2026-01-26 18:56:07 +0530

    Time: 4 ms (18.93%), Space: 44.9 MB (6.1%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.java
+++ 1544-make-the-string-great/1544-make-the-string-great.java
@@ -1,17 +1,23 @@
 class Solution {
+    public boolean ispossible(char ch1 , char ch2){
+        if(ch1 == ch2) return false;
+        ch1 = Character.toLowerCase(ch1);
+        ch2 = Character.toLowerCase(ch2);
+
+        if(ch1 == ch2) return true;
+        return false ;
+    }
     public String makeGood(String s) {
-        StringBuilder sb = new StringBuilder();
-        int pt2 = 0;
-        for(int i = 0; i<s.length(); i++){
-            if(pt2>0 && Math.abs(sb.charAt(pt2-1) - s.charAt(i)) == 32){
-                pt2--;
-                sb.deleteCharAt(pt2);
-            }
-            else{
-                sb.append(s.charAt(i));
-                pt2++;
-            }
+        int n =s.length();
+        String result ="";
+        for(int i =0; i<n;i++){
+            char ch = s.charAt(i);
+            int len = result.length();
+            if(len > 0 && ispossible(ch,(result.charAt(len-1)))){
+                result =result.substring(0,len-1);
+
+            }else result = result.concat(String.valueOf(ch));
         }
-        return sb.toString();
+        return result ;
     }
 }
