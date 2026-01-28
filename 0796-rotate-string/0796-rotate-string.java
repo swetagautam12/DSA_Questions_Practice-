3d0aaff3742748e913b45cc812b3b9c2a4e5adc2
Sweta Gautam <sg060479@gmail.com>
2026-01-28 19:07:37 +0530

    Time: 0 ms (100%), Space: 42.8 MB (50.66%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean rotateString(String s, String goal) {
+
+        if(s.length() !=goal.length()){
+            return false ;
+
+        }
+        String name = s+s;
+        return name.contains(goal);
+        
+    }
+}
