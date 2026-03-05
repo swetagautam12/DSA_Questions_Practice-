4f87450265eb6bf87b75f5e7a7a27359075f7e30
Sweta Gautam <sg060479@gmail.com>
2026-03-05 23:50:03 +0530

    Time: 0 ms (100%), Space: 47.6 MB (30.97%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xor1 = 0;
+        int xor2 = 0;
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            xor2 = xor2 ^ nums[i];
+            xor1 = xor1 ^ (i + 1);
+        }
+        return xor1 ^ xor2;
+    }
+}
