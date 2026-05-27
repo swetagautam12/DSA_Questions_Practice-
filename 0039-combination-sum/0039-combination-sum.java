277f2894514ee345d35898af8d56f33ba3e0833a
Sweta Gautam <sg060479@gmail.com>
2026-05-27 22:48:30 +0530

    Time: 2 ms (94.81%), Space: 45.7 MB (49.57%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,27 +1,26 @@
 class Solution {
-    public static void solve(List<Integer> currList, List<List<Integer>> ans, int[] nums, int target, int index, int currSum){
-        if(index>nums.length-1){
-            return;
-        }
-        if(currSum==target){
-            ans.add(new ArrayList<>(currList));
-            return;
-        }
-
-        //pick
-        if(currSum+ nums[index]<=target){
-            currList.add(nums[index]);
-            solve(currList, ans, nums, target, index, currSum+nums[index]);
-            currList.remove(currList.size()-1);
-        }
-
-        //not pick
-        solve(currList, ans, nums, target, index+1, currSum);
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        BackTracking(0, candidates, target, res, new ArrayList<>());
+        return res;
     }
-    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> currList = new ArrayList<>();
-        solve(currList, ans, candidates, target,0, 0);
-        return ans;
+
+    public static void BackTracking(int i, int[] arr, int target,
+                                    List<List<Integer>> list,
+                                    List<Integer> curr) {
+        if (i == arr.length) {
+            if (target == 0) {
+                list.add(new ArrayList<>(curr));
+            }
+            return;
+        }
+
+        if (arr[i] <= target) {
+            curr.add(arr[i]);
+            BackTracking(i, arr, target - arr[i], list, curr);
+            curr.remove(curr.size() - 1);
+        }
+
+        BackTracking(i + 1, arr, target, list, curr);
     }
 }
